d853e7c359e6c1cc61ca756019836eef928e3e5b
snakeyworm <calebwloera@gmail.com>
2021-12-09 15:27:40 -0800

    readline

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,20 +2,34 @@
 // Node.js - Hello World!
 
 const http = require( "http" )
-const args = require( "minimist" )( process.argv.slice( 2 ) )
 
 const hostname = "127.0.0.1"
 const port = 3000
-process.exitCode = 1
 
-const server = http.createServer( ( req, res ) => { 
-    res.statusCode = 200
+const readline = require( "readline" ).createInterface( {
+    input: process.stdin,
+    output: process.stdout,
+} )
+
+
+const server = http.createServer( ( req, res ) => {
+    res.status = 200
     res.setHeader( "Content-Type", "text/plain" )
-    res.end( "Hello World!" )
+    readline.question( "Do you wan't to play game? ", answer => {
+        let joshuaAnswer
+        switch ( answer[0] ) {
+            case "y":
+                joshuaAnswer = "How about Global Thermal Nuclear War?"
+                break
+            case "n":
+                joshuaAnswer = "Okay bye"
+                break
+        }
+        readline.close()
+        res.end( joshuaAnswer )
+    } )
 } )
 
 server.listen( port, hostname, () => {
     console.log( `Server runing at http://${hostname}:${port}` )
 } )
-
-
