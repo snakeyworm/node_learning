48ff6516872fd6ec53dbe9ff06e10b38f309f0cf
snakeyworm <calebwloera@gmail.com>
2021-12-14 11:28:14 -0800

    Renaming

--- esm_module/main.js
+++ esm_module/main.js
@@ -1,2 +1,12 @@
 
+import fetch from "node-fetch"
 
+function getUser( name ) {
+    return fetch( "http://localhost:8080/users.json" )
+        .then( res => res.json() )
+        .then( res => fetch( `http://localhost:8080/users/${res[name]}.json` ) )
+        .then( res => res.json() )
+}
+
+getUser( "caleb" )
+    .then( res => console.log( res ) )
